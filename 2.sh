e2ab52b769ef2956174461d6a9b539ecf6d1f583
InYourG00D1 <44290386+InYourG00D1@users.noreply.github.com>
2019-02-27 22:38:03 +0700

    Add files via upload

---
+++ 2.sh
@@ -0,0 +1,113 @@
+#!/system/xbin/bash 
+#Maafkan Kegabutan Ku Ster :V
+#bwahaha
+
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+
+echo $red "Ragil Itu Ganss 😎😍"
+sleep 1
+clear
+echo $blue "Ragil Itu Ganss 😎😍"
+sleep 1
+clear
+echo $green "Ragil Itu Ganss 😎😍"
+sleep 1
+clear
+echo $red "Ragil Itu Ganss 😎😍"
+sleep 
+clear
+echo $blue "Ragil Itu Ganss 😎😍"
+sleep 1
+clear
+echo $green "Ragil Itu Ganss 😎😍"
+sleep 1
+clear
+echo $red "Ragil Itu Ganss 😎😍"
+sleep 1
+clear
+echo $blue "Ragil Itu Ganss 😎😍"
+sleep 1
+clear
+echo $green "Lu Buriq"
+sleep 1
+clear
+
+echo "\033[1;31m]
+                (()))
+               /|x x|   
+              /\( - ) 
+      ___.-._/\/  
+     /=*_*-*-*/ !! 
+    |-{-_-_-} ! 
+    (-{-_-_-} ! 
+     \{_-_-_} ! 
+      }-_-_-} 
+      {-_|-_} 
+      {-_|_-} 
+      {_-|-_} 
+      {_-|-_} 
++=====%%@ @%%==================+
++ coded by : InYourG00D        +
++ Team : LNX#CREW S.T.C BUFT   +
++ kontak me : 6281382406878    +
++==============================+
+  ^...^                   ^...^
+ <_* *_> KoutaFree(Tree) <_* *_> 
+   \_/                     \_/   ";
+echo $green"MenuPaketanFree";
+echo $blue"1.30GB (Tree)";
+echo $purple"2.10GB (Tree)";
+echo $yellow"3.2.75GB (Tree)";
+echo $green"4.5GB (Tree)"
+echo $red"00.Ini namanya Out"
+read -p "[>>" stc
+
+if [ $stc = 1 ]
+then 
+clear
+echo "MasukanNomerMu"|lolcat
+read no
+echo "Wait 5menit"|lolcat
+echo "$no, Sukses Gan"
+fi
+
+if [ $stc = 2 ]
+then 
+clear
+echo "MasukanNomerMu"|lolcat
+read no
+echo "Wait 5menit"|lolcat
+echo "$no, Sukses Gan"|lolcat
+fi
+
+if [ $stc = 3 ]
+then 
+clear
+echo "MasukanNomerMu"|lolcat
+read no
+echo "Wait 5menit"|lolcat
+echo "$no, Sukses Gan"|lolcat
+fi
+
+if [ $stc = 4 ]
+then 
+clear
+echo "MasukanNomerMu"|lolcat
+read no
+echo "Wait 5menit"|lolcat
+echo "$no, Sukses Gan"|lolcat
+fi
+
+if [ $stc = 00 ]
+then 
+clear
+echo "Apakah Ragil Gans?"|lolcat
+read ganss
+echo "$ganss, Tq Gan"|lolcat
+fi
